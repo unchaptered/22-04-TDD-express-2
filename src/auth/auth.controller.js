3e8de5794b191f9ff21ebfcf241eec9bbbe38c43
Unchaptered <workstation19961002@gmail.com>
2022-04-25 00:08:11 +0900

    ADD republish token controller
    토큰 재발행 기능이 추가되며, 테스트가 완료된 미들웨어들을 연결하였습니다.

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -3,6 +3,8 @@ import * as authService from './auth.service';
 import JwtModule from '../token/jwt.module';
 import ResFormFactory from '../factories/res.form.factory';
 
+import { redisConnection, redisRefreshExpired } from '../options/redis.option';
+
 // GET /auth 로그인
 export const login = async (req, res) => {
 
@@ -18,8 +20,10 @@ export const login = async (req, res) => {
             const resJson = ResFormFactory.getFailureForm('The password don\'t match');
             return res.status(400).json(resJson);
         }
-        const accessToken = JwtModule.encodeAccessToken({ email: account.email });
+        const accessToken = JwtModule.encodeAccessToken({ _id:account._id, email: account.email });
         const refreshToken = JwtModule.encodeRefreshToken();
+        
+        await redisConnection.set(account._id.toString(), refreshToken, { EX: redisRefreshExpired });
         
         const resJson = ResFormFactory.getSuccessForm('The password match', { account, accessToken, refreshToken });
         return res.status(201).json(resJson);
@@ -40,7 +44,6 @@ export const join = async (req, res) => {
             const resJson = ResFormFactory.getFailureForm(`The ${email} is already taken`);
             return res.status(409).json(resJson);
         }
-
         
         const account = await authService.joinUser(email, password);
         const resJson = ResFormFactory.getSuccessForm(`The ${email} is created`, account);
@@ -53,7 +56,28 @@ export const join = async (req, res) => {
 
 }
 
-export const getAccessToken = () => {
+export const reGetAccessToken = async (req, res) => {
+    
+    try {
+        const { _id, email } = req.body;
+        const redisToken = await redisConnection.get(_id);
+
+        const refreshTokenEncrypted= req.headers.refresh;
+        const refreshToken = refreshTokenEncrypted.split('Bearer ')[1];
+        if (redisToken !== refreshToken) {
+            const resJson = ResFormFactory.getFailureForm('Your token is invalid, please re-login');
+            return res.status(403).json(resJson);
+        }
+
+        const accessToken = JwtModule.encodeAccessToken({ _id, email });
+        const resJson = ResFormFactory.getSuccessForm('Your access token is republish', { accessToken, refreshToken });
+        return res.status(201).json(resJson);   
+        
+    } catch (error) {
+        const resJson = ResFormFactory.getFailureForm({ name: error.name, message:error.message });
+        return res.status(500).json(resJson);
+    }
+
 }
 
 export const getProfile = () => {
